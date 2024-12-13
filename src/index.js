7f9073dbfb9f718a94114d2b8a2284faa14134af
PiyushGhavghave <piyushghavghave@gmail.com>
2024-12-14 01:38:03 +0530

    initial setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv'
+dotenv.config({
+    path : './.env'
+})
+import connectDB from "./db/connect_DB.js";
+import {app} from './app.js';
+const port = process.env.PORT || 8000;
+
+connectDB()
+.then(() => {
+    app.listen(port, () => {
+        console.log(`Appliaction is sreving on port ${port}`);
+    })
+})
+.catch((err) => {
+    console.log("Mongodb Connection failed : ", err);
+})
+
