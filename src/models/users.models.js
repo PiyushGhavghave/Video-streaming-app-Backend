71444e109411241f5d07a24fdd0289d0bd71beb7
PiyushGhavghave <piyushghavghave@gmail.com>
2024-12-14 21:02:25 +0530

    hashed password using bcrypt and gnerated access , refresh token using JWT

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,4 +1,6 @@
 import mongoose,{Schema} from "mongoose";
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 const userSchema = new Schema({
     username : {
@@ -44,5 +46,45 @@ const userSchema = new Schema({
     }
 }, {timestamps: true})
 
+//Pre hook : will invoke "BEFORE" execution .... in this case "save"
+// whenever user save data.... this hook will invoke function
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+// Add additional methods 
+userSchema.methods.validatePassword = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname : this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    )
+}
 
 export const User = mongoose.model("User", userSchema)
