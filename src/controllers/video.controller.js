57e4e9cc3a10802a620b17e491dd6abd28ebe948
PiyushGhavghave <piyushghavghave@gmail.com>
2024-12-28 21:42:24 +0530

    (small change) in video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -264,7 +264,7 @@ const deleteVideo = asyncHandler(async (req, res) => {
 
     return res.status(200)
     .json(
-        new apiResponse(200, video, "Video deleted successfully")
+        new apiResponse(200, {}, "Video deleted successfully")
     )
 
 })
